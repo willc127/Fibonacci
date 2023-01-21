80f23b583aeda1d7a0aa8da8eff6998d1bd1b2a4
William Cesar <williamcesar.ribeiro@outlook.com>
2023-01-21 11:10:59 -0300

    Drawing of spiral

--- Spiral.py
+++ Spiral.py
@@ -1,47 +1,49 @@
 import turtle as t
-import tkinter as _
 
-#Specify pencolor and screensize dimension
-t.screensize(canvwidth=700, canvheight=500, bg="black")
-t.pencolor('lightgreen')
-t.width(2)
+#Create function to draw the Fibonacci sequence
+def drawFibonacci(terms, scale):
+    #Specify screensize dimension
+    scr = t.Screen()
+    scr.setup(width = 1.0, height = 1.0)
+    
+    #Specify backgorun and pen colors
+    t.bgcolor('black')
+    t.pencolor('lightgreen')
+    t.width(2)
 
-#Specify speed of drawing; 0 = fastest
-t.speed(0)
+    #Specify speed of drawing; 0 = fastest
+    t.speed(0)
 
-#Define terms of Fibonacci's sequence
-terms = 10
+    #Define initial values of Fibonacci's sequence
+    previous = 0
+    start = 1
 
-#Specify scale for drawing
-scale = 5
+    #Calculate Fibonacci's sequence
+    for i in range(0, terms-1):
+        fib = previous + start
+        for n in range(0,6): #Draw a square and ends in initial position of next square
+            t.forward(fib*scale) 
+            t.left(90)
+        t.right(90)
+        previous = start #update previous value
+        start = fib
 
-#Define initial values of Fibonacci's sequence
-previous = 0
-start = 1
+    #Stop drawing and go to origin
+    t.penup()
+    t.home()
 
-#Calculate Fibonacci's sequence
-for i in range(0, terms-1):
-    fib = previous + start
-    for n in range(0,6): #Draw a square and ends in initial position of next square
-        t.forward(fib*scale) 
-        t.left(90)
-    t.right(90)
-    previous = start #update previous value
-    start = fib
+    #Start next drawing (Spiral)
+    t.pendown()
+    previous = 0
+    start = 1
 
-#Stop drawing and go to origin
-t.penup()
-t.home()
+    for i in range(0, terms-1):
+        fib = previous + start
+        t.circle(fib*scale,90) #Draw a quarter of circle
+        previous = start
+        start = fib
 
-#Start next drawing (Spiral)
-t.pendown()
-previous = 0
-start = 1
-
-for i in range(0, terms-1):
-    fib = previous + start
-    t.circle(fib*scale,90) #Draw a quarter of circle
-    previous = start
-    start = fib
+#Call function to draw
+drawFibonacci(14,1)
 
 t.done()
