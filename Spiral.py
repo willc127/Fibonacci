8bdc916429b2c16674a56ef4141a0dd625374b7e
William Cesar <williamcesar.ribeiro@outlook.com>
2023-04-17 23:54:45 -0300

    Figures fixed

--- Spiral.py
+++ Spiral.py
@@ -1,49 +1,52 @@
 import turtle as t
 
-#Create function to draw the Fibonacci sequence
+# Create function to draw the Fibonacci sequence
+
+
 def drawFibonacci(terms, scale):
-    #Specify screensize dimension
+    # Specify screensize dimension
     scr = t.Screen()
-    scr.setup(width = 1.0, height = 1.0)
-    
-    #Specify backgorun and pen colors
+    scr.setup(width=1.0, height=1.0)
+
+    # Specify backgorun and pen colors
     t.bgcolor('black')
     t.pencolor('lightgreen')
     t.width(2)
 
-    #Specify speed of drawing; 0 = fastest
-    t.speed(0)
+    # Specify speed of drawing; 0 = fastest
+    t.speed(10)
 
-    #Define initial values of Fibonacci's sequence
+    # Define initial values of Fibonacci's sequence
     previous = 0
     start = 1
 
-    #Calculate Fibonacci's sequence
+    # Calculate Fibonacci's sequence
     for i in range(0, terms-1):
         fib = previous + start
-        for n in range(0,6): #Draw a square and ends in initial position of next square
-            t.forward(fib*scale) 
+        for n in range(0, 6):  # Draw a square and ends in initial position of next square
+            t.forward(fib*scale)
             t.left(90)
         t.right(90)
-        previous = start #update previous value
+        previous = start  # update previous value
         start = fib
 
-    #Stop drawing and go to origin
+    # Stop drawing and go to origin
     t.penup()
     t.home()
 
-    #Start next drawing (Spiral)
+    # Start next drawing (Spiral)
     t.pendown()
     previous = 0
     start = 1
 
     for i in range(0, terms-1):
         fib = previous + start
-        t.circle(fib*scale,90) #Draw a quarter of circle
+        t.circle(fib*scale, 90)  # Draw a quarter of circle
         previous = start
         start = fib
 
-#Call function to draw
-drawFibonacci(14,1)
+
+# Call function to draw
+drawFibonacci(14, 1)
 
 t.done()
